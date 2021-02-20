50ecbffdbbf4491ead580c3c9a3de4389ae7b42b
Henriquepb22 <henriquepbdev@gmail.com>
2021-02-20 11:57:19 -0300

    Remove cra trash and install required dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <h1>Teste Frontend - 2SOW</h1>
+            </header>
+        </div>
+    )
+}
+
+export default App
